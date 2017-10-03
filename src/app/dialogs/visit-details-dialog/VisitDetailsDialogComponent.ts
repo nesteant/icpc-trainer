00a2eac209f911c28244e77b63130139e20ce0de
Anton Nesterenko <nesterenko.anton@gmail.com>
2017-10-03 20:39:34 +0300

    FIX: fixed views

--- src/app/dialogs/visit-details-dialog/VisitDetailsDialogComponent.ts
+++ src/app/dialogs/visit-details-dialog/VisitDetailsDialogComponent.ts
@@ -1,5 +1,5 @@
-import {Component, Inject, Input, OnInit} from '@angular/core';
-import {MD_DIALOG_DATA, MdDialogRef} from '@angular/material';
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, Input} from '@angular/core';
+import {MD_DIALOG_DATA, MdDialogRef, MdTab} from '@angular/material';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/mergeMap';
 import {SubVisit} from '../../model/SubVisit';
@@ -7,19 +7,17 @@ import {SubVisit} from '../../model/SubVisit';
 @Component({
   selector: 'icpc-visit-details-dialog',
   templateUrl: 'visit-details-dialog.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class VisitDetailsDialogComponent implements OnInit {
+export class VisitDetailsDialogComponent {
 
   @Input()
   public visit: SubVisit;
 
   constructor(public dialogRef: MdDialogRef<VisitDetailsDialogComponent>,
+              public cd: ChangeDetectorRef,
               @Inject(MD_DIALOG_DATA) public data: any) {
     this.visit = data.visit;
   }
-
-  public ngOnInit() {
-  }
-
 
 }
