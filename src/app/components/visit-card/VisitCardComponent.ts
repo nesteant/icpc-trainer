f051240ce3cb371dad19c05d74cf2e2d0eab023a
Anton Nesterenko <nesterenko.anton@gmail.com>
2017-10-28 13:47:11 +0300

    FIX: added clickable options to items

--- src/app/components/visit-card/VisitCardComponent.ts
+++ src/app/components/visit-card/VisitCardComponent.ts
@@ -1,6 +1,8 @@
 import {Component, Input} from '@angular/core';
 import {Episode} from '../../model/Episode';
 import {SubVisit} from '../../model/SubVisit';
+import {VisitDetailsDialogComponent} from '../../dialogs/visit-details-dialog/VisitDetailsDialogComponent';
+import {MatDialog} from '@angular/material';
 
 @Component({
   selector: 'icpc-visit-card',
@@ -11,4 +13,17 @@ export class VisitCardComponent {
   public visit: { date: string, values: SubVisit[] };
   @Input()
   public episode: Episode;
+
+  constructor(public dialog: MatDialog) {
+  }
+
+  public openVisitDetailsModal(visit: SubVisit) {
+    let dialogRef = this.dialog.open(VisitDetailsDialogComponent, {
+      height: '600px',
+      width: '700px',
+      data: {
+        visit: visit
+      }
+    });
+  }
 }
--- src/app/components/visit-card/VisitCardModule.ts
+++ src/app/components/visit-card/VisitCardModule.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../../SharedModule';
 import {CommonModule} from '@angular/common';
 import {VisitCardComponent} from './VisitCardComponent';
+import {SubVisitCardModule} from '../subvisit-card/SubVisitCardModule';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    SubVisitCardModule
   ],
   exports: [
     VisitCardComponent
